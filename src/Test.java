c3f75296a777cb645cc3c43a69064453f74635e1
bhargavak <bhargavak37@gmail.com>
2017-08-19 08:56:32 +0530

    excluded unwanted files

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,14 @@
  */
 public class Test {
     public static void main(String[] args) {
-        int [] a = new int [] {23,67,12,6,90,34}; // Test data to check the code
-     double max=a[0];
-     for(int i=1;i<a.length;i++)
-         if(a[i]>max) max=a[i];
+       findMax();
+    }
+    public  static void findMax()
+    {
+        int [] a = new int [] {23,67,12,6,55,34,80,30,10,98}; // Array of size 10 elements
+        double max=a[0];
+        for(int i=1;i<a.length;i++)
+            if(a[i]>max) max=a[i];
         System.out.println(max);
     }
 }
