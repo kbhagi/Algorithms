3d70487690f3c519ef9640fd6e1c7fabffcc4237
bhargavak <bhargavak37@gmail.com>
2017-08-19 09:10:28 +0530

    explaination of algorithm as comments

--- src/Test.java
+++ src/Test.java
@@ -3,16 +3,27 @@
  */
 public class Test {
     public static void main(String[] args) {
-       findMax();
+        int[] a = new int[]{23, 67, 12, 6, 55, 34, 80, 30, 10, 98};      // Array of size 10 elements
+        findMax(a);
     }
 
-    private static void findMax()
+    private static void findMax(int[] a)
     {
-        int [] a = new int [] {23,67,12,6,55,34,80,30,10,98}; // Array of size 10 elements
+
         double max = a[0]; //take first element as max element
         for (int i = 1; i < a.length; i++) //use a for loop to go trhough all elements of array
             if (a[i] > max)
                 max = a[i]; // if an element is greater than max element, assign that element to max variable
         System.out.println(max);// print out the variable
     }
+
+    private static void computeAverage(int[] a) {
+        int N = a.length;//size of array is the total number of elements
+        double sum = 0.0;//default value for sum
+        for (int i = 0; i < N; i++)
+            sum += a[i];//keeping adding all elements to sum
+        double average = sum / N;//divide sum by size of array , total number of array elements
+    }
+
+
 }
