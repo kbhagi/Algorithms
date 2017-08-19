e6836a2bf4133fe1f058e18a896fddd9e36e5138
bhargavak <bhargavak37@gmail.com>
2017-08-19 09:01:28 +0530

    explaination of algorithm as coments

--- src/Test.java
+++ src/Test.java
@@ -5,12 +5,14 @@ public class Test {
     public static void main(String[] args) {
        findMax();
     }
-    public  static void findMax()
+
+    private static void findMax()
     {
         int [] a = new int [] {23,67,12,6,55,34,80,30,10,98}; // Array of size 10 elements
-        double max=a[0];
-        for(int i=1;i<a.length;i++)
-            if(a[i]>max) max=a[i];
-        System.out.println(max);
+        double max = a[0]; //take first element as max element
+        for (int i = 1; i < a.length; i++) //use a for loop to go trhough all elements of array
+            if (a[i] > max)
+                max = a[i]; // if an element is greater than max element, assign that element to max variable
+        System.out.println(max);// print out the variable
     }
 }
